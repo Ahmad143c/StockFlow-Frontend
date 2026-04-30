9425bbb724b71fda11748c262540be6e9fea3609
Ahmad Affaq <ahmadaffaq12@gmail.com>
2026-04-30 06:46:58 +0000

    fix: add missing SearchIcon import in AdminProductList.jsx

--- src/pages/AdminProductList.jsx
+++ src/pages/AdminProductList.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchProducts } from '../redux/productsSlice';
 import { Grid, Card, CardContent, Typography, CardHeader, CardMedia, Fade, Box, Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField, MenuItem, IconButton, InputAdornment, Paper, Divider, Tooltip, Chip, FormControl, InputLabel, Select } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import SearchIcon from '@mui/icons-material/Search';
 import API from '../api/api';
 import { useDarkMode } from '../context/DarkModeContext';
 import { Alert } from '@mui/material';
